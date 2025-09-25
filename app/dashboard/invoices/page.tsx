bd0228a83b48b22983f4223c8c6b107fc079826b
Rawlin <rawlinleitao08@gmail.com>
2025-09-25 22:15:36 +0530

    v8 - Deploy using docker

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { Metadata } from 'next';
+//export const dynamic = "force-dynamic";
+
 
 export const metadata: Metadata = {
   title: 'Invoices',
